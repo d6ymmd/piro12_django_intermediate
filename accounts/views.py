f04dab2ce14842121b8ce973cc7e2760b4ba6742
bora shin <d6ymmd@gmail.com>
2020-01-22 23:09:28 +0900

    EP08: signup & next GET

--- accounts/views.py
+++ accounts/views.py
@@ -14,12 +14,14 @@ from .forms import SignupForm
 #         if form.is_valid():
 #             user = form.save()
 #             auth_login(request, user)
-#             return redirect('profile')
+#             next_url = request.GET.get('next') or 'profile'
+#             return redirect(next_url)
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html', {
-#         'form':form,
+#         'form': form,
 #     })
+
 
 class SignupView(CreateView):
     model = User
@@ -27,7 +29,8 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
